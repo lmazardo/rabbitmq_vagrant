d646414fc44828b74889afcdedd3f6b5fbec1a14
Pascal Grange <pascal1.grange@orange.com>
2014-09-29 17:45:20 +0200

    ADD boot two rabbit nodes and join them in a cluster.

--- Vagrantfile
+++ Vagrantfile
@@ -8,13 +8,16 @@ Vagrant::Config.run do |config|
       puppet.manifest_file = "rabbit.pp"
     end
     rabbit1_config.vm.network :hostonly, "192.168.1.10"
+    rabbit1_config.vm.host_name = "rabbit1"
   end
-  #config.vm.define :rabbit1 do |rabbit1_config|
-  #  rabbit1_config.vm.box = "puppetlabs/debian-7.4-64-puppet"
-  #  rabbit1_config.vm.provision :puppet do |puppet|
-  #    puppet.manifest_file = "rabbit.pp"
-  #  end
-  #  rabbit1_config.vm.network :hostonly, "192.168.1.10"
-  #end
+  config.vm.define :rabbit2 do |rabbit2_config|
+    rabbit2_config.vm.box = "puppetlabs/debian-7.4-64-puppet"
+    rabbit2_config.vm.provision :puppet do |puppet|
+      puppet.manifest_file = "rabbit.pp"
+    end
+    rabbit2_config.vm.network :hostonly, "192.168.1.11"
+    rabbit2_config.vm.host_name = "rabbit2"
+  end
 
 end
+#puppet apply --manifestdir /tmp/vagrant-puppet-1/manifests --detailed-exitcodes /tmp/vagrant-puppet-1/manifests/rabbit.pp
--- manifests/rabbit.pp
+++ manifests/rabbit.pp
@@ -1,16 +1,38 @@
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 
 class system-update {
-  exec { 'apt-get update':
+  exec { 'apt-key present rabbit}':
+    command => "/usr/bin/wget -q http://www.rabbitmq.com/rabbitmq-signing-key-public.asc -O -|/usr/bin/apt-key add -",
+    unless  => "/usr/bin/apt-key list|/bin/grep -c 056E8E56",
+  } -> file {'/etc/apt/sources.list.d/rabbitmq.list':
+    content => 'deb http://www.rabbitmq.com/debian/ testing main',
+  } -> exec { 'apt-get update':
     command => 'apt-get update',
+  } -> file {'/etc/hosts':
+    content => "
+192.168.1.10 rabbit1
+192.168.1.11 rabbit2
+",
   }
 }
 
 class rabbit() {
   package {'rabbitmq-server':
     ensure => present,
+    notify => Exec['purge first rabbit launch'],
+  } -> file {'/var/lib/rabbitmq/.erlang.cookie':
+    content => 'IDURWQDPCZHKVIFHXXOF',
+    notify => Service['rabbitmq-server'],
+  } -> file {'/etc/rabbitmq/rabbitmq.config':
+    ensure => present,
+    content => "[{rabbit, [{cluster_nodes, {['rabbit@rabbit1', 'rabbit@rabbit2'], disc}}]}].",
+    notify => Service['rabbitmq-server'],
   }
-  service { 'rabbitmq-server':
+
+  exec{ 'purge first rabbit launch':
+    command => 'rabbitmqctl stop_app; rabbitmqctl reset; killall epmd beam; echo OK',
+    refreshonly => true,
+  } -> service{ 'rabbitmq-server':
     ensure => running,
   }
 }
@@ -23,5 +45,3 @@ stage { 'first':
 class { 'system-update':
   stage => first,
 }
-
-
