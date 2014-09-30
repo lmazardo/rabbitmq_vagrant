59ab0b4f479a7b976d7619be7e4dde3af05afbe9
Pascal Grange <pascal1.grange@orange.com>
2014-09-30 15:46:07 +0200

    ADD define a global ha policy

--- manifests/rabbit.pp
+++ manifests/rabbit.pp
@@ -27,6 +27,9 @@ class rabbit() {
     ensure => present,
     content => "[{rabbit, [{loopback_users, []}, {cluster_nodes, {['rabbit@rabbit1', 'rabbit@rabbit2'], disc}}]}].",
     notify => Service['rabbitmq-server'],
+  } -> exec { 'set rabbit ha policy':
+    command => 'rabbitmqctl set_policy ha ".*" \'{"ha-mode":"all"}\'',
+    unless => "rabbitmqctl list_policies  | cut -f2 | grep '^ha$'" 
   }
 
   exec{ 'purge first rabbit launch':
